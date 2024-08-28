53627eb8102d5f9b1232ee2c2b36217f509dfc99
Gustavo Homem <homem.gustavo@gmail.com>
2024-08-28 14:09:49 +0200

    add deploy script

---
+++ deploy.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# load variables from configuration file
+. conf/vars.env
+
+# load functions from function library file
+. lib/functions
+
+### Main script ###
+
+my_id=$(id -u)
+
+test $my_id -eq 0
+handle_error $? "Please run this script as root."
+
+echo "Deploying to $DJANGO_HOMEDIR/random_project"
+
+# copy the application to the destination directory
+rsync -a --delete ./$DJANGO_PROJ_NAME $DJANGO_HOMEDIR/
+
+# make sure all the content belongs do DJANGO_USERNAME
+chown -R $DJANGO_USERNAME:$DJANGO_USERNAME $DJANGO_HOMEDIR
+
+# print user friendly output
+print_output
