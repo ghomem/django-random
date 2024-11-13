6c0b2c811f585d4419197957dd7712c388ea7d1d
guillegrc <guillermo.garcia@meaningfuldata.eu>
2024-11-13 15:23:46 +0000

    show user statistics

--- random_project/random_app/urls.py
+++ random_project/random_app/urls.py
@@ -3,8 +3,9 @@ from django.conf import settings
 from . import views
 
 urlpatterns = [
-    path("",               views.index,          name=settings.APP_NAME),
-    path("get_number",     views.get_number,     name="get_number"),
-    path("review",         views.review,         name="review"),
-    path("delete_history", views.delete_history, name="delete_history"),
+    path("",                views.index,           name=settings.APP_NAME),
+    path("get_number",      views.get_number,      name="get_number"),
+    path("review",          views.review,          name="review"),
+    path("delete_history",  views.delete_history,  name="delete_history"),
+    path("user_statistics", views.user_statistics, name="user_statistics"),
 ]
