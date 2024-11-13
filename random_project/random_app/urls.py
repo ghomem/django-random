40dff37137d6776458a3a410c7567eef6cb4a75b
guillegrc <guillermo.garcia@meaningfuldata.eu>
2024-11-13 12:12:52 +0000

    kept column style in urls

--- random_project/random_app/urls.py
+++ random_project/random_app/urls.py
@@ -3,8 +3,8 @@ from django.conf import settings
 from . import views
 
 urlpatterns = [
-    path("",               views.index,      name=settings.APP_NAME),
-    path("get_number",     views.get_number, name="get_number"),
-    path("review",         views.review,     name="review"),
+    path("",               views.index,          name=settings.APP_NAME),
+    path("get_number",     views.get_number,     name="get_number"),
+    path("review",         views.review,         name="review"),
     path("delete_history", views.delete_history, name="delete_history"),
 ]
