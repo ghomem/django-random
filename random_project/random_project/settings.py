af8291f8bbc776abdcab7a9b27715e52aaa78e23
Guillermo <guillermo.garcia@meaningfuldata.eu>
2024-11-19 11:39:28 +0000

    added autogenerated secret key

--- random_project/random_project/settings.py
+++ random_project/random_project/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+from django.core.management.utils import get_random_secret_key
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-kl(6)m^no$-+qprbvjx!zx_k59hyiy-hhtl=n#3(avkh66zk+&'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', get_random_secret_key())
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
