e8f32360770fa7c6cc4753ea29c207ea93511f86
Guillermo <guillermo.garcia@meaningfuldata.eu>
2024-11-19 11:38:08 +0000

    set debug to false

--- random_project/random_project/settings.py
+++ random_project/random_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-kl(6)m^no$-+qprbvjx!zx_k59hyiy-hhtl=n#3(avkh66zk+&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [ '127.0.0.1', 'localhost' ]
 
 
 # Application definition
